4380cca812354661c1d034759b42e52cf152183b
casjay <git-admin@casjaysdev.com>
2020-09-06 01:20:36 -0400

    Create mongodb.sh

---
+++ mongodb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p /var/lib/docker/storage/mongodb/ && chmod -Rf 777 /var/lib/docker/storage/mongodb
+
+sudo docker run -d \
+--name=mongodb \
+-p 27017:27017 \
+-v /var/lib/docker/storage/mongodb/mongodb:/data/db \
+-e TZ=America/New_York \
+--restart unless-stopped \
+mongo:latest
